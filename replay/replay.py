e051db1ab123b39c7f790eb2a55db884d9d887b8
Tom Punihaole <tom.punihaole@gmail.com>
2019-12-02 15:55:39 -0800

    Handle any webhook body or data type

--- replay/replay.py
+++ replay/replay.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import getopt
+import argparse
 import sys
+import base64
 
 import requests
 
@@ -8,16 +9,19 @@ from rehook import RehookGateway
 from rehook.webhook import Webhook
 
 
-def usage(cmd):
-    return '%s -p <path to replay> -t <target url>' % cmd
-
-
 def replay_webhook(session: requests.Session, webhook: Webhook, target: str):
+    kwargs = {}
+    if webhook.body_base64:
+        kwargs['data'] = base64.b64decode(webhook.body_base64)
+    elif webhook.body:
+        kwargs['data'] = webhook.body
+    elif webhook.data:
+        kwargs['json'] = webhook.data
     req = requests.Request(method=webhook.method,
                            url=target,
                            headers=webhook.headers,
                            params=webhook.query_params,
-                           data=webhook.post_data)
+                           **kwargs)
     response = session.send(req.prepare())
     if response.status_code >= 400:
         print(f'Error replaying webhook {webhook}: {response}.')
@@ -26,35 +30,33 @@ def replay_webhook(session: requests.Session, webhook: Webhook, target: str):
 
 
 def main(cmd, argv):
-    try:
-        opts, args = getopt.getopt(argv, 'hp:t:', ['path=', 'target=', ])
-    except getopt.GetoptError:
-        print(usage(cmd))
-        sys.exit(1)
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-p', '--path', required=True)
+    arg_parser.add_argument('-t', '--target', required=True)
+    arg_parser.add_argument('--cleanup', dest='cleanup', action='store_true')
+    arg_parser.add_argument('--pause', dest='pause', action='store_true')
 
-    source = None
-    target = None
+    args = arg_parser.parse_args()
 
-    for opt, arg in opts:
-        if opt == '-h':
-            print(usage(cmd))
-            sys.exit(0)
-        elif opt in ('-p', '--path'):
-            source = arg
-        elif opt in ('-t', '--target'):
-            target = arg
-
-    rehook = RehookGateway()
-    webhooks = rehook.webhooks.list()
+    gateway = RehookGateway()
     replayed = 0
     success = 0
+    removed = 0
     session = requests.Session()
-    for webhook in webhooks:
-        if webhook.path == source:
-            replayed += 1
-            if replay_webhook(session, webhook, target):
-                success += 1
+    for webhook in gateway.webhooks.search().filter(path=args.path).execute():
+        if args.pause:
+            print(webhook)
+            input(f'Press Enter to confirm replay to {args.target}: ')
+        replayed += 1
+        if replay_webhook(session, webhook, args.target):
+            success += 1
+        if args.cleanup:
+            if not gateway.webhooks.delete(webhook.id):
+                print(f'Error cleaning up webhook {webhook}.')
+            else:
+                removed += 1
     print(f'Replayed {replayed} webhooks.')
+    print(f'Removed {removed} webhooks.')
 
 
 if __name__ == '__main__':
