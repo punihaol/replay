0fe4141b59d39480b97986924d0e28a991c3b38a
Tom Punihaole <tom.punihaole@gmail.com>
2021-05-25 12:22:40 -0500

    Add support for recurly.

--- replay/replay.py
+++ replay/replay.py
@@ -34,50 +34,55 @@ def replay_webhook(session: requests.Session, webhook: Webhook, target: str):
 
 def main(cmd, argv):
     arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-x', '--host', required=False)
     arg_parser.add_argument('-p', '--path', required=True)
     arg_parser.add_argument('-t', '--target', required=True)
     arg_parser.add_argument('--cleanup', dest='cleanup', action='store_true')
     arg_parser.add_argument('--pause', dest='pause', action='store_true')
     arg_parser.add_argument('-f', '--filter', help='filter out hooks..provide url formatted query parameters with keys (id, provider, raw_type, date, user_name, user_id) operators =, <, >, :in-> and value following...ex. id=83zq6w&raw_type:in->subscription_canceled,subscription_went_active')
-    arg_parser.add_argument('--ls', dest='ls', action='store_true', help='list out all hooks (list is generated post filter)')
 
     args = arg_parser.parse_args()
-
-    gateway = RehookGateway()
+    gw_kwargs = {}
+    if args.host:
+        host_str = args.host
+        if host_str.startswith(':'):
+            host, port = None, host_str[1:]
+        elif ':' in host_str:
+            host, port = host_str.split(':')
+        else:
+            host, port = host_str, None
+        if host:
+            gw_kwargs['host'] = host
+        if port:
+            gw_kwargs['port'] = port
+    gateway = RehookGateway(**gw_kwargs)
     replayed = 0
     success = 0
     removed = 0
     session = requests.Session()
-    webhooks =gateway.webhooks.search().filter(path=args.path).execute()
+    webhooks = gateway.webhooks.search().filter(path=args.path).execute()
     # used just for data filtering / listing
-    webhooksNormalized = list(map(lambda x: NormalizedWebhookData(x), webhooks))
     if args.filter is not None:
-        origCount = len(list(webhooksNormalized))
-        webhooksNormalized = list(filter(lambda x: x.meets_criteria(args.filter), webhooksNormalized))
+        webhooks_normalized = list(map(lambda x: NormalizedWebhookData(x), webhooks))
+        orig_count = len(list(webhooks_normalized))
+        webhooks_normalized = list(filter(lambda x: x.meets_criteria(args.filter), webhooks_normalized))
         # adjust webhooks if going to do replay..
-        webhooks = list(map(lambda x: x.raw_webhook, webhooksNormalized))
-        print(f'\nFiltered down to {len(webhooksNormalized)}/{origCount} hooks..')
-    
-    if args.ls:
-        print('\n')
-        for webhook in webhooksNormalized:
-            print(webhook.to_str_row())
-        print('\n')
-    else:
-        for webhook in webhooks:
-            if args.pause:
-                print(webhook)
-                input(f'Press Enter to confirm replay to {args.target}: ')
-            replayed += 1
-            if replay_webhook(session, webhook, args.target):
-                success += 1
-            if args.cleanup:
-                if not gateway.webhooks.delete(webhook.id):
-                    print(f'Error cleaning up webhook {webhook}.')
-                else:
-                    removed += 1
-        print(f'Replayed {replayed} webhooks.')
-        print(f'Removed {removed} webhooks.')
+        webhooks = list(map(lambda x: x.raw_webhook, webhooks_normalized))
+        print(f'\nFiltered down to {len(webhooks_normalized)}/{orig_count} hooks..')
+    for webhook in webhooks:
+        if args.pause:
+            print(webhook)
+            input(f'Press Enter to confirm replay to {args.target}: ')
+        replayed += 1
+        if replay_webhook(session, webhook, args.target):
+            success += 1
+        if args.cleanup:
+            if not gateway.webhooks.delete(webhook.id):
+                print(f'Error cleaning up webhook {webhook}.')
+            else:
+                removed += 1
+    print(f'Replayed {replayed} webhooks.')
+    print(f'Removed {removed} webhooks.')
 
 if __name__ == '__main__':
     main(sys.argv[0], sys.argv[1:])
